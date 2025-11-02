299c8ef30506c9511793879e12addfab15ba43af
Haitao Feng <haitao.feng@gmail.com>
2025-11-03 06:46:23 +0800

    Pre install mcp-server-fetch

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -4,3 +4,6 @@ DOCKER_IMAGE_SOURCE_TAG=oh_v0.59.0_if48c2wny33soysh_w8u9ct7lxv15vcdg
 
 DOCKER_IMAGE_TAG=oh_v0.59.0_image_nikolaik_s_python-nodejs_tag_python3.12-nodejs22
 DOCKER_IMAGE_SOURCE_TAG=oh_v0.59.0_if48c2wny33soysh_w8u9ct7lxv15vcdg
+
+DOCKER_IMAGE_TAG=oh_v0.59.0_image_nikolaik_s_python-nodejs_tag_python3.12-nodejs22
+DOCKER_IMAGE_SOURCE_TAG=oh_v0.59.0_if48c2wny33soysh_f598sn44x8cjrvi0
