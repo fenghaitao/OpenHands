1d88c7c6aaa830cea246e0d165fe971473010f85
Haitao Feng <haitao.feng@gmail.com>
2025-11-02 19:49:35 +0800

    Add containers/runtime/config.sh

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -1,10 +1,6 @@
-DOCKER_REGISTRY=ghcr.io
-DOCKER_ORG=all-hands-ai
-DOCKER_BASE_DIR="./containers/runtime"
-DOCKER_IMAGE=runtime
-# These variables will be appended by the runtime_build.py script
-# DOCKER_IMAGE_TAG=
-# DOCKER_IMAGE_SOURCE_TAG=
 
 DOCKER_IMAGE_TAG=oh_v0.59.0_image_nikolaik_s_python-nodejs_tag_python3.12-nodejs22
-DOCKER_IMAGE_SOURCE_TAG=oh_v0.59.0_if48c2wny33soysh_b6j8asr5tetga6ch
+DOCKER_IMAGE_SOURCE_TAG=oh_v0.59.0_if48c2wny33soysh_w8u9ct7lxv15vcdg
+
+DOCKER_IMAGE_TAG=oh_v0.59.0_image_nikolaik_s_python-nodejs_tag_python3.12-nodejs22
+DOCKER_IMAGE_SOURCE_TAG=oh_v0.59.0_if48c2wny33soysh_w8u9ct7lxv15vcdg
